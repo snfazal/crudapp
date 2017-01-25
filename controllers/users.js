f6c6d0e5c119175d0d039645a2925c54ef926b2c
snfazal <snfazal@gmail.com>
2017-01-25 14:26:56 -0500

    added get route to users.js

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,14 @@ var User = require('../models/user.js');
 var authHelpers = require('../helpers/auth.js')
 
 router.get('/signup', function(req, res){
-});
+  User.find({})
+  .exec(function(err, users){
+    if (err) { console.log(err); }
+    res.render('users/index.hbs', {
+                users: users
+              })
+  });
+})
 
 router.post('/', authHelpers.createSecure, function(req, res){
 });
