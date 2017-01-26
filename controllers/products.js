8705c8d9557a9be637beb3e20c3977c04e9d3d21
snfazal <snfazal@gmail.com>
2017-01-25 20:15:52 -0500

    update route added, not working

--- controllers/products.js
+++ controllers/products.js
@@ -3,3 +3,14 @@ var router        = express.Router();
 var User          = require('..models/users.js');
 var Product       = require('..models/users.js');
 var authHelpers   = require('../helpers/auth.js');
+
+
+
+//Product update SHOW route
+router.get('/:id/edit/:pId', authHelpers.authorize, function(req, res){
+  Brew.findById(req.params.pId)
+  .exec(function(err, brew){
+    if (err) {console.log(err);}
+    res.send(brew);
+  });
+});
