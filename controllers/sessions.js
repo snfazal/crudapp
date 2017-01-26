7fbdc27ee4b25b48b7d8042e06b998f106cc975a
snfazal <snfazal@gmail.com>
2017-01-25 20:04:38 -0500

    added sessions routes, user login, user logout

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,13 +4,24 @@ var User = require('../models/user.js');
 var authHelpers = require('../helpers/auth.js')
 
 router.get('/login', function(req, res) {
-  res.render('users/login.hbs')
+  res.render('login page');
 })
 
 router.post('/login', authHelpers.loginUser, function(req, res){
+  User.findOne({
+    email: req.body.email,
+    password_digest: req.body.password
+  })
+  .exec(function(err, user){
+    if (err) {console.log(err);}
+    res.send(req.session);
+  });
 });
 
 router.delete('/', function(req, res){
-})
+  req.session.destroy(function(){
+    res.send(req.session);
+  });
+});
 
 module.exports = router;
