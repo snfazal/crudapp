ef66d74b2e2652821198964458246464296c771a
snfazal <snfazal@gmail.com>
2017-01-26 11:57:40 -0500

    database running, created a homepage

--- controllers/products.js
+++ controllers/products.js
@@ -9,10 +9,18 @@ var authHelpers   = require('../helpers/auth.js');
 //Product update SHOW route
 router.get('/:id/edit/:pId', authHelpers.authorize, function(req, res){
   Product.findById(req.params.pId)
-  .exec(function(err, brew){
+  .exec(function(err, product){
     if (err) {console.log(err);}
-    res.send(brew);
+    res.send(product);
   });
 });
 
 //product delete
+router.put('/:id/:pId', authHelpers.authorize, function(req, res){
+  Product.findByIdAndUpdate(req.params.pId, req.body)
+  .exec(function(err, product){
+    if (err) {console.log(err);}
+    product.save();
+    res.redirect{`/products/${req.params.id}/${req.params.pId}`};
+  });
+});
--- server.js
+++ server.js
@@ -6,13 +6,14 @@ var logger = require('morgan');
 var hbs = require('hbs')
 var mongoose = require('mongoose');
 
+//require the controllers so that they render onthe page
 var usersController = require('./controllers/users.js');
 var sessionsController = require('./controllers/sessions.js');
 
 var app = express();
 
 // ADD THE NAME OF YOUR DATABASE
-// mongoose.connect('mongodb://localhost/<YourDatabaseNameHere>');
+mongoose.connect('mongodb://localhost/skin_app');
 
 app.set('view engine', 'hbs')
 
@@ -30,4 +31,21 @@ app.use(session({
 app.use('/users', usersController);
 app.use('/sessions', sessionsController);
 
-app.listen(4000);
+// Now that we're connected, let's save that connection to the database in a variable.
+var db = mongoose.connection;
+
+// Will log an error if db can't connect to MongoDB
+db.on('error', function(err){
+  console.log(err);
+});
+
+// Will log "database has been connected" if it successfully connects.
+db.once('open', function(){
+  console.log('database connected!');
+});
+
+app.listen(4000, function(){
+  console.log('============');
+  console.log('WERK, WERK');
+  console.log('============');
+});
