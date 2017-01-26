9ad0eec454fae634fa4af2bdde2fa256571df72c
snfazal <snfazal@gmail.com>
2017-01-25 20:13:03 -0500

    required all helpers for products.js

---
+++ controllers/products.js
@@ -0,0 +1,5 @@
+var express       = require('express');
+var router        = express.Router();
+var User          = require('..models/users.js');
+var Product       = require('..models/users.js');
+var authHelpers   = require('../helpers/auth.js');
